fb4913dc0463b3ab10532eaa1d914fbef2e5af05
yu00hun <yu00hun@naver.com>
2024-01-01 18:03:58 +0900

    Title: 친근하지 않은 수, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240101/친근하지 않은 수/unfriendly-number.py
+++ 240101/친근하지 않은 수/unfriendly-number.py
@@ -2,7 +2,7 @@ n = int(input())
 cnt = 0
 
 for i in range(1, n+1):
-    if i % 2 != 0 and i % 3 != 0 and i % 5 != 0:
+    if i % 2 == 0 or i % 3 == 0 or i % 5 == 0:
         continue
     cnt += 1
         
