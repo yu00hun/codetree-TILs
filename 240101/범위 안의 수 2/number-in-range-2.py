c033e1c1dbaeee65b2f5b1d7ea42ab3ae8128866
yu00hun <yu00hun@naver.com>
2024-01-01 17:24:39 +0900

    Title: 범위 안의 수 2, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240101/범위 안의 수 2/number-in-range-2.py
@@ -0,0 +1,13 @@
+sum = 0
+cnt = 0
+avg = 0
+
+for _ in range(10):
+    a = int(input())
+    if a >= 0 and a <= 200:
+        sum += a
+        cnt += 1
+        avg = sum / cnt
+
+
+print(f"{sum} {avg:.1f}")
