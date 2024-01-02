723add017438bd6b255ba4f4e512bc9ae145e52f
yu00hun <yu00hun@naver.com>
2024-01-02 16:17:23 +0900

    Title: 3n + 1 수열, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240102/3n + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,15 @@
+n = int(input())
+cnt = 0
+
+while True:
+    if n == 1:
+        break
+
+    if n % 2 == 0:
+        n = n // 2
+    else:
+        n = n * 3 + 1
+        
+    cnt += 1
+
+print(cnt)
