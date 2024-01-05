5b93afeac8b6d4ebdbfdc177b450db69ce0ab374
yu00hun <yu00hun@naver.com>
2024-01-05 17:13:16 +0900

    Title: 공약수의 유무, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240105/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240105/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -3,7 +3,7 @@ a, b = int(inp[0]), int(inp[1])
 satisfied = False
 
 for i in range(a, b+1):
-    if 1920 % i == 0 or 2880 % i == 0:
+    if 1920 % i == 0 and 2880 % i == 0:
         satisfied = True
 
 if satisfied:
