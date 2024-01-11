1c3734b856ddabfd73ce6b67c0d3ccb2c642f71b
yu00hun <yu00hun@naver.com>
2024-01-11 16:38:14 +0900

    Title: 정상적인 구구단, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/정상적인 구구단/normal-multiple.py
+++ 240111/정상적인 구구단/normal-multiple.py
@@ -2,8 +2,7 @@ n = int(input())
 
 for i in range(n):
     for j in range(n):
-        if j == n-1:
-            print(f"{i+1} * {j+1} = {(i+1)*(j+1)}", end=" ")
-        else:
-            print(f"{i+1} * {j+1} = {(i+1)*(j+1)},", end=" ")
+        print(f"{i+1} * {j+1} = {(i+1)*(j+1)}", end="")
+        if j != n-1:
+            print(",", end=" ")
     print()
