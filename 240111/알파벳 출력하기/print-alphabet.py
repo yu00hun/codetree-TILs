ae8e1793c54774d03dec71c1f7a35ca1d3b038eb
yu00hun <yu00hun@naver.com>
2024-01-11 19:59:20 +0900

    Title: 알파벳 출력하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/알파벳 출력하기/print-alphabet.py
+++ 240111/알파벳 출력하기/print-alphabet.py
@@ -3,8 +3,8 @@ char = 'A'
 
 for i in range(n):
     for j in range(i+1):
-        if chr(ord(char)-1) == 'Z':
-            char = 'A'
         print(char, end="")
         char = chr(ord(char)+1)
+        if ord(char) > ord('Z'):
+            char = 'A'
     print()
