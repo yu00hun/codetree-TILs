157974805a61f0cb8b0c6b347ebb1d10a0b17eff
yu00hun <yu00hun@naver.com>
2024-01-11 16:43:24 +0900

    Title: 구구단 만들기, Time: 231ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240111/구구단 만들기/print-multiplication-table.py
@@ -0,0 +1,9 @@
+inp = input().split()
+a, b = int(inp[0]), int(inp[1])
+
+for i in range(1, 10):
+    for j in range(b, a-1, -2):
+        print(f"{j} * {i} = {j*i}", end=" ")
+        if j != a:
+            print("/", end=" ")
+    print()
