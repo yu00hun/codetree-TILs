322c68df922ebe579cf7820cc99a2f342f2314cf
yu00hun <yu00hun@naver.com>
2024-01-13 17:52:00 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240113/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240113/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -1,13 +1,17 @@
 n = int(input())
 arr = list(map(int, input().split()))
-max_val = 0
-idx = 0
+prev_max_idx = n
 
 while True:
-    max_val = max(arr)
-    idx = arr.index(max_val)
-    print(idx+1, end=" ")
-    arr = arr[:idx]
+    max_idx = 0
 
-    if idx == 0:
+    for i in range(1, prev_max_idx):
+        if arr[i] > arr[max_idx]:
+            max_idx = i
+            
+    print(max_idx + 1, end=" ")
+
+    if max_idx == 0:
         break
+
+    prev_max_idx = max_idx
