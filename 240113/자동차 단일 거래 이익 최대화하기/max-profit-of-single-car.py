5ca6e92b30c511d7888ace1419f0602d01a8e2f0
yu00hun <yu00hun@naver.com>
2024-01-13 18:40:17 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240113/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240113/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -4,7 +4,7 @@ max_val = 0
 
 for i in range(0, n-1):
     for j in range(i+1, n):
-        if arr[j] - arr[i] > 0 and arr[j] - arr[i] > max_val:
+        if arr[j] - arr[i] > max_val:
             max_val = arr[j] - arr[i]
 
 print(max_val)
