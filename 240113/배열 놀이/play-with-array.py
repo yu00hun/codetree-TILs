5cff8c171b2a29baf4de004a772faa860e19ddc5
yu00hun <yu00hun@naver.com>
2024-01-13 16:17:04 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240113/배열 놀이/play-with-array.py
+++ 240113/배열 놀이/play-with-array.py
@@ -9,7 +9,15 @@ for i in range(q):
     elif q_arr[0] == 2:
         if q_arr[1] in n_arr:
             print(n_arr.index(q_arr[1])+1)
-            continue
+        else:
+            print(0)
     elif q_arr[0] == 3:
         for i in range(q_arr[1], q_arr[2]+1):
             print(n_arr[i-1], end=" ")
+
+1 24
+2 85
+2 98
+59
+31
+0
