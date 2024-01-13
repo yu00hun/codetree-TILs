30e66b7371107ce47e619ff457237f22d9191faa
yu00hun <yu00hun@naver.com>
2024-01-13 15:55:10 +0900

    Title: 나눗셈의 나머지, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240113/나눗셈의 나머지/remainder-of-division.py
+++ 240113/나눗셈의 나머지/remainder-of-division.py
@@ -2,13 +2,11 @@ a, b = tuple(map(int, input().split()))
 count_arr = [0] * 10
 ans = 0
 
-for i in range(100):
+while a > 1:
     count_arr[a%b] += 1
     a = a // b
-    
-    if a <= 1:
-        break
-        
+
+
 for elem in count_arr:
     ans += elem * elem
 
