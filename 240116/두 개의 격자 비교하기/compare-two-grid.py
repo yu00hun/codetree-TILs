9c59cf9d1231ea2223da32262dd6a0214a0d19f8
yu00hun <yu00hun@naver.com>
2024-01-16 17:49:25 +0900

    Title: 두 개의 격자 비교하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/두 개의 격자 비교하기/compare-two-grid.py
+++ 240116/두 개의 격자 비교하기/compare-two-grid.py
@@ -12,6 +12,10 @@ arr_3 = [
     [0 for _ in range(m)]
     for _ in range(n)
 ]
+# arr_3 = [
+# 	[1 if arr_1[i][j] != arr_2[i][j] else 0 for j in range(m)]
+# 	for i in range(n)
+# ]
 
 for i in range(n):
     for j in range(m):
