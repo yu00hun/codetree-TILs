dc1aba0bb64c90bb0f965998a5f1d7a589ca0f0a
yu00hun <yu00hun@naver.com>
2024-01-16 16:52:55 +0900

    Title: 배열의 합, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/배열의 합/sum-of-array.py
+++ 240116/배열의 합/sum-of-array.py
@@ -1,4 +1,9 @@
-for _ in range(4):
-    arr = list(map(int, input().split()))
-    result = sum(arr)
-    print(result)
+# 2차원 배열을 구현해 각 줄마다 정수를 입력받습니다.
+arr_2d = [
+	list(map(int, input().split()))
+	for _ in range(4)
+]
+	
+# 각 줄의 합을 출력합니다.
+for i in range(4):
+	print(sum(arr_2d[i]))
