9de98a32d543e1935cfc5f3e7a09c17f291c3e5d
yu00hun <yu00hun@naver.com>
2024-01-17 16:32:11 +0900

    Title: 격자 모양 출력하기, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240117/격자 모양 출력하기/print-grid-shape.py
@@ -0,0 +1,15 @@
+n, m = tuple(map(int, input().split()))
+
+arr = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+for i in range(m):
+    r, c = tuple(map(int, input().split()))
+    arr[r-1][c-1] = r*c
+
+for row in arr:
+    for elem in row:
+        print(elem, end=" ")
+    print()
