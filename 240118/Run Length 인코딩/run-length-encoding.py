352398516e7e9f796c7843b04513a4e6ca7b7efc
yu00hun <yu00hun@naver.com>
2024-01-18 15:13:26 +0900

    Title: Run Length 인코딩, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/Run Length 인코딩/run-length-encoding.py
+++ 240118/Run Length 인코딩/run-length-encoding.py
@@ -1,23 +1,29 @@
-string = input()
+# 변수 선언 및 입력:
+A = input()
 
-list = []
-cnt = 1
+# 변환
+encoded = ""
 
-for i in range(len(string)-1):
-
-    if string[i] == string[i+1]:
-        cnt += 1
-        if i == len(string) - 2:
-            list.append(f"{string[i]}{cnt}")
-        
+# 입력의 첫번째 값을 읽고 초기화합니다.
+curr_char = A[0]
+num_char = 1
+for traget_char in A[1:]:
+    if traget_char == curr_char:
+        # 지금까지의 문자와 같으면 연속된 문자 개수만 추가해 주고 넘어갑니다.
+        num_char += 1
     else:
-        list.append(f"{string[i]}{cnt}")
-        cnt = 1
+        # 지금까지의 문자와 다르면 새로운 문자로 바꿔줘야 합니다.
+        # 지금까지 세어온 curr_char와 num_char를 기록합니다.
+        encoded += curr_char
+        encoded += str(num_char)
 
-leng = 0
-for i in range(len(list)):
-    leng += len(list[i])
-print(leng)
+        # curr_char와 num_char를 현재 값으로 초기화합니다.
+        curr_char = traget_char
+        num_char = 1
+    
+# 마지막 덩어리에 해당하는 curr_char와 num_char를 기록합니다.
+encoded += curr_char
+encoded += str(num_char)
 
-for elem in list:
-    print(elem, end="")
+print(len(encoded))
+print(encoded)
