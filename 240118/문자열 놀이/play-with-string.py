dde293f741d029317108ed2acefcd5f09fbb6f71
yu00hun <yu00hun@naver.com>
2024-01-18 17:10:44 +0900

    Title: 문자열 놀이, Time: 147ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240118/문자열 놀이/play-with-string.py
@@ -0,0 +1,22 @@
+s, q = tuple(input().split())
+arr = list(s)
+q = int(q)
+
+for i in range(q):
+    n, a, b = tuple(input().split())
+    n = int(n)
+
+    if n == 1:
+        a, b = int(a), int(b)
+        for j in range(len(s)):
+            tmp = arr[a-1]
+            arr[a-1] = arr[b-1]
+            arr[b-1] = tmp
+
+    elif n == 2:
+        for j in range(len(s)):
+            if arr[j] == a:
+                arr[j] = b
+
+    answer = ''.join(arr)
+    print(answer)
