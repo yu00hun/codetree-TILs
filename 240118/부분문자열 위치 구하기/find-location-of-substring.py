dcf1f241ee46deffc41c90144ae290a4f555761f
yu00hun <yu00hun@naver.com>
2024-01-18 16:38:54 +0900

    Title: 부분문자열 위치 구하기, Time: 260ms, Memory: 73MB, Status: Passed - CodeTree

--- 240118/부분문자열 위치 구하기/find-location-of-substring.py
+++ 240118/부분문자열 위치 구하기/find-location-of-substring.py
@@ -1,4 +1,17 @@
 string = input()
 target = input()
 
-print(string.find(target))
+# print(string.find(target))
+
+for i in range(len(string)-len(target)+1):
+    is_matched = True
+    for j in range(len(target)):
+        if string[i+j] != target[j]:
+            is_matched = False
+
+    if is_matched:
+        print(i)
+        break
+
+if is_matched == False:
+    print(-1)
