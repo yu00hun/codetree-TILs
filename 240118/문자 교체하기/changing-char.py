a0ccd812ac69ac6b504e7646518c5a748d05ed94
yu00hun <yu00hun@naver.com>
2024-01-18 16:51:42 +0900

    Title: 문자 교체하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/문자 교체하기/changing-char.py
+++ 240118/문자 교체하기/changing-char.py
@@ -1,9 +1,5 @@
-first, second = tuple(input().split())
+first, second = input().split()
 
-arr_first = list(first)
-arr_second = list(second)
-
-arr = arr_first[:2] + arr_second[2:]
-answer = ''.join(arr)
+answer = first[:2] + second[2:]
 
 print(answer)
