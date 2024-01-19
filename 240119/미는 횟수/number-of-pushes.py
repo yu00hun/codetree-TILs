c1997609fc945e8aa938174f69a4774cb2583783
yu00hun <yu00hun@naver.com>
2024-01-19 19:39:17 +0900

    Title: 미는 횟수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240119/미는 횟수/number-of-pushes.py
+++ 240119/미는 횟수/number-of-pushes.py
@@ -4,12 +4,12 @@ len_A = len(A)
 cnt = -1
 
 for _ in range(len_A):
-    A = A[-1] + A[:-1]
-    cnt += 1
     if A == B:
         break
+    A = A[-1] + A[:-1]
+    cnt += 1
 
-if cnt == 0:
+if cnt == len_A:
     print(-1)
 else:
     print(cnt+1)
